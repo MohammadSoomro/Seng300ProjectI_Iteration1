09ade09a2ff8941ecca1bd7c32613cc8c9940342
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2023-10-30 18:16:38 -0600

    Completed implementation of AddItemViaScan
    Completed implementation of AddItemViaScan (Testing still required, and further debugging)

--- eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
+++ eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
@@ -1,6 +1,10 @@
 package com.thelocalmarketplace.software;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import com.jjjwelectronics.Mass;
+import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.scale.AbstractElectronicScale;
 import com.jjjwelectronics.scale.ElectronicScale;
 import com.jjjwelectronics.scanner.Barcode;
@@ -9,7 +13,9 @@ import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
 
 public class Main {
-
+	
+	private static ArrayList<BarcodedProduct> shoppingCart = new ArrayList();
+	private static SelfCheckoutStation station = new SelfCheckoutStation();
 
 	/**
 	 * Details about the item to add must be provided to the system  CHECK
@@ -21,29 +27,32 @@ public class Main {
 	/**
 	 * @param a barcode given by listeners of barcodeScanner
 	 * @return 
+	 * @throws OverloadedDevice 
 	 */
-	public static void addItemViaScan(Barcode barcode) {
+	public static void addItemViaScan(Barcode barcode) throws OverloadedDevice {
+		boolean skipBagging = false;
+		Scanner myScanner = new Scanner(System.in);
 		// get BarcodedProduct mass from database
 		BarcodedProduct product = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
 		
 		// update expected weight in bagging area
 		Mass productMass = new Mass(product.getExpectedWeight());
-		Mass currentWeightInBaggingArea = null;// ElectronicScale.getCurrentMassOnTheScale();
-		Mass expectedWeightInBaggingArea = productMass.sum(currentWeightInBaggingArea);
+		Mass currentWeightInBaggingArea = station.baggingArea.getCurrentMassOnTheScale();
+		Mass expectedWeightInBaggingArea = currentWeightInBaggingArea.sum(productMass);
 		
-		// blocks system from further adds until product is placed in bagging area
-		/**
-		 * while currentWeightInBaggingArea != expectedWeightInBaggingArea {
-		 * 
-		 * 	display message "please place item in bagging area"
-		 * 
-		 * 	display:
-		 * 	skipBaggingButton(){
-		 * 		expectedWeightInBaggingArea = currentWeightInBaggingArea;
-		 * 	}
-		 * 
-		 * }
-		 */
+		// blocks system from further adds until product is placed in bagging area or bagging is skipped
+		while (currentWeightInBaggingArea != expectedWeightInBaggingArea) {
+			System.out.print("please place item in bagging area");
+			System.out.print("Skip bagging?(enter 1 for yes): ");
+			String response = myScanner.nextLine();
+			if (response == "1") {
+				skipBagging = true;
+				expectedWeightInBaggingArea = currentWeightInBaggingArea;
+			}
+		}
+		shoppingCart.add(product);
+		
+		
 	}
 
 }
