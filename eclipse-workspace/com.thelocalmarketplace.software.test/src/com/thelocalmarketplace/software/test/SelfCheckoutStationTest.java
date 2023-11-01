46df522cac9ae8881a09765318bdbccbffa0eebd
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2023-11-01 13:49:55 -0600

    added instance of new selfcheckoutsystem
    added instance of selfcheckoutsystem to setup test

--- eclipse-workspace/com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationTest.java
+++ eclipse-workspace/com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationTest.java
@@ -3,11 +3,13 @@ package com.thelocalmarketplace.software.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.thelocalmarketplace.hardware.SelfCheckoutStation;
+
 public class SelfCheckoutStationTest {
 
 	@Before
 	void setup() {
-		
+		SelfCheckoutStation station = new SelfCheckoutStation();
 	}
 	
 	
