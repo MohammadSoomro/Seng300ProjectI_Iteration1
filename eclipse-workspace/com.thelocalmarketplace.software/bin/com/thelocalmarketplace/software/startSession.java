649859ed215855ac7b4266f52ff9d56f52a07e1b
ChristianN517 <98929417+ChristianN517@users.noreply.github.com>
2023-11-01 10:05:46 -0600

    Added totalPrice Method, started working on startSession functionality and added theoretical for loop for listening for coin intake

--- eclipse-workspace/com.thelocalmarketplace.software/bin/com/thelocalmarketplace/software/startSession.java
+++ eclipse-workspace/com.thelocalmarketplace.software/bin/com/thelocalmarketplace/software/startSession.java
@@ -12,7 +12,7 @@ import java.util.ArrayList;
 import com.thelocalmarketplace.hardware.BarcodedProduct;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 
-public class StartSession {
+public class startSession {
 	
 	private boolean inSession = false;
 	private int Totalcost = 0;
@@ -27,11 +27,40 @@ public class StartSession {
 		this.inSession = inSession;
 	}
 	
+	//Calculates total price of items in a user's shopping cart
+	public float totalPrice(ArrayList<BarcodedProduct> itemsFromCart) {
+		int i= 0;
+		float sumOfItemPrices = 0;
+		while (i <= itemsFromCart.size()) {
+			float price = itemsFromCart[i].price();
+			sumOfItemPrices += price;
+			i++;
+		}
+		
+		return sumOfItemPrices;
+	}
+	
 	public void startSession() {
 		shoppingCart.clear();
 		this.totalWeight=0;
 		this .Totalcost=0;
 		this.inSession = true;
+		
+		
+		//Logic code for what happens when customer chooses to pay via coin
+		
+		float totalPrice = totalPrice(shoppingCart);
+		
+		//initialize listeners in a loop
+		
+		//should expect invalidCoinException, NullCoinException, others?
+		while (totalPrice > 0) {
+			
+			// listener.listens
+			//newMoney = listener.returned
+			//totalPrice -= newMoney
+		}
+		
 	}
 	
 	public void endSession() {
