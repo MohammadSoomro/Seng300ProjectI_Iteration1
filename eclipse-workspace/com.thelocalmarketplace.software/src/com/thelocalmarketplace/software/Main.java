6e7faf44460dbd2819a1750483165035a723d956
Dongwen <tdongwen@gmail.com>
2023-11-02 16:37:02 -0600

    added getExpectedMass

--- eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
+++ eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
@@ -2,10 +2,31 @@ package com.thelocalmarketplace.software;
 
 import java.util.ArrayList;
 
+import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.scale.ElectronicScale;
+import com.thelocalmarketplace.hardware.BarcodedProduct;
 
 
 public class Main {
+	
+	
+	private ArrayList<BarcodedProduct> shoppingCart = new ArrayList<>();
+	
+	/**
+	 * Obtains the expected mass in the bagging area.
+	 * 
+	 * @return The expected mass.
+	 */
+	public Mass getExpectedMass() {
+		Mass expectedMass = new Mass(0);
+		
+		for (BarcodedProduct items : shoppingCart) {
+			 expectedMass.sum(new Mass(items.getExpectedWeight() * Mass.MICROGRAMS_PER_GRAM));
+		}
+		
+		return expectedMass;
+	}
+	
 	
 
 }
