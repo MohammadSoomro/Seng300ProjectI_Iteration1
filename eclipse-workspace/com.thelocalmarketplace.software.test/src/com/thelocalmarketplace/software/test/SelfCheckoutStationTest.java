6e43b4756c6d33adebabac9bee72c549216217ae
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2023-11-04 16:26:46 -0600

    Simplified method, trigger from listener will trigger addItem event

--- eclipse-workspace/com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationTest.java
+++ eclipse-workspace/com.thelocalmarketplace.software.test/src/com/thelocalmarketplace/software/test/SelfCheckoutStationTest.java
@@ -3,18 +3,22 @@ package com.thelocalmarketplace.software.test;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.jjjwelectronics.scale.ElectronicScaleListener;
 import com.thelocalmarketplace.hardware.SelfCheckoutStation;
 
 public class SelfCheckoutStationTest {
 
 	@Before
 	void setup() {
+		ElectronicScaleListener ScaleListener = null;
 		SelfCheckoutStation station = new SelfCheckoutStation();
+		station.baggingArea.register(ScaleListener);
 	}
 	
 	
 	@Test
-	void tempTest(){
+	void AddValidProductToCart(){
+		
 		
 	}
 }
--- eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
+++ eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
@@ -3,20 +3,29 @@ package com.thelocalmarketplace.software;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import com.jjjwelectronics.IDevice;
+import com.jjjwelectronics.IDeviceListener;
+import com.jjjwelectronics.Item;
 import com.jjjwelectronics.Mass;
 import com.jjjwelectronics.OverloadedDevice;
 import com.jjjwelectronics.scale.AbstractElectronicScale;
 import com.jjjwelectronics.scale.ElectronicScale;
 import com.jjjwelectronics.scale.ElectronicScaleListener;
+import com.jjjwelectronics.scale.IElectronicScale;
 import com.jjjwelectronics.scanner.Barcode;
+import com.jjjwelectronics.scanner.BarcodeScannerListener;
+import com.jjjwelectronics.scanner.IBarcodeScanner;
 import com.thelocalmarketplace.hardware.*;
 import com.thelocalmarketplace.hardware.external.ProductDatabases;
 
 
-public class Main {
+public class Main implements ElectronicScaleListener, BarcodeScannerListener{
 	
 	private static ArrayList<BarcodedProduct> shoppingCart = new ArrayList();
 	private static SelfCheckoutStation station = new SelfCheckoutStation();
+	private boolean inSession = true;
+	private static Mass zeroMass = new Mass(0);
+	private static Mass tempMass = zeroMass;
 
 	/**
 	 * Details about the item to add must be provided to the system  CHECK
@@ -26,38 +35,113 @@ public class Main {
 	 */
 	
 	/**
+	 * @return 
+	 * 
+	 */
+	@Override
+	public void theMassOnTheScaleHasChanged(IElectronicScale scale, Mass mass) {
+		if (!scale.isDisabled() & inSession) {
+			
+			double tempWeight = 0;
+			
+			for (BarcodedProduct products : shoppingCart) 
+				tempWeight += products.getExpectedWeight();
+			
+			Mass expectedMass = new Mass(tempWeight * Mass.MICROGRAMS_PER_GRAM);
+			
+			if (mass.difference(expectedMass).abs().compareTo(scale.getSensitivityLimit()) >= 0) {
+				station.scanner.disable();
+				station.coinSlot.disable();
+			}
+			tempMass = mass;
+		}		
+	}
+	
+	@Override
+	public void aBarcodeHasBeenScanned(IBarcodeScanner barcodeScanner, Barcode barcode) {
+		addItemViaScan(barcode);
+	}
+	/**
 	 * @param a barcode given by listeners of barcodeScanner
 	 * @return 
 	 * @throws OverloadedDevice 
 	 */
-	public static void addItemViaScan(Barcode barcode) throws OverloadedDevice {
-		boolean skipBagging = false;
-		Scanner myScanner = new Scanner(System.in);
+	public static void addItemViaScan(Barcode barcode) {
+
 		// get BarcodedProduct mass from database
 		BarcodedProduct product = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
+		Mass productMass = new Mass(product.getExpectedWeight());
 		
 		// update expected weight in bagging area
-		Mass productMass = new Mass(product.getExpectedWeight());
-		Mass currentWeightInBaggingArea = station.baggingArea.getCurrentMassOnTheScale();
+		Mass currentWeightInBaggingArea = null;
+		try {
+			currentWeightInBaggingArea = station.baggingArea.getCurrentMassOnTheScale();
+		} catch (OverloadedDevice e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		Mass expectedWeightInBaggingArea = currentWeightInBaggingArea.sum(productMass);
 		
-		// blocks system from further adds until product is placed in bagging area or bagging is skipped
-		while (currentWeightInBaggingArea != expectedWeightInBaggingArea) {
-			System.out.print("please place item in bagging area");
-			System.out.print("Skip bagging?(enter 1 for yes): ");
-			String response = myScanner.nextLine();
-			if (response == "1") {
-				skipBagging = true;
-				// unblock system
-				expectedWeightInBaggingArea = currentWeightInBaggingArea;}
-			// detect weight change on scale
-			//else if(weightDiscrepency = false) {
-			//  // unblock system
-			//	expectedWeightInBaggingArea = currentWeightInBaggingArea
-			//}
-		}
+		// add item to cart
 		shoppingCart.add(product);
 		
+		/**
+		 *  blocks system from further adds until
+		 *  product is placed in bagging area or bagging is skipped (weight discrepancy is solved)
+		 */
+		while (tempMass != expectedWeightInBaggingArea) {
+			System.out.print("please place item in bagging area");
+			// notify attendant station
+			notifyAttendantStation();
+		}
+		tempMass = zeroMass;
+	}
+		
+
+	private static void notifyAttendantStation() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void theMassOnTheScaleHasExceededItsLimit(IElectronicScale scale) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void theMassOnTheScaleNoLongerExceedsItsLimit(IElectronicScale scale) {
+		// TODO Auto-generated method stub
 		
 	}
 
