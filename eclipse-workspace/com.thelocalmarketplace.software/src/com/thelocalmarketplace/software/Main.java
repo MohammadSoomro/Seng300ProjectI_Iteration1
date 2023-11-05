8674e08250acd285a35ff1568af330e3b8cc54b0
Gurjit Samra <104379797+Gurjit-Samra@users.noreply.github.com>
2023-11-04 22:37:27 -0600

    Updated Main.java
    added commits from AddItemViaScan

--- eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
+++ eclipse-workspace/com.thelocalmarketplace.software/src/com/thelocalmarketplace/software/Main.java
@@ -1,3 +1,15 @@
+/**
+* @author Gurjit Samra : 30172814
+* @author Mohammad Soomro : 30130440
+* @author Christian Nielson: 30185718
+* @author Ajaypal Sallh: 30023811
+* @author Dongwen Tian: 30181813
+*
+*/
+
+
+
+
 package com.thelocalmarketplace.software;
 
 import java.util.ArrayList;
@@ -16,7 +28,52 @@ public class Main implements ElectronicScaleListener {
 	
 	protected static ArrayList<BarcodedProduct> shoppingCart;
 	protected static boolean inSession;
+	boolean skipBagging = false;
+	/**
+	 * This method represents when the customer scans an item.
+	 * The item is added to the items list.
+	 * 
+	 * @param item The item that was scanned
+	 */
+	@Override
+	public synchronized void scan(BarcodedItem item) {
+		super.scan(item);
+		if(skipBagging) {
+			station.baggingArea.removeAnItem(item);
+			notifyAttendantStation();
+		}
+		station.baggingArea.addAnItem(item);
+	}
 	
+	/**
+	 * This method is called when an item has been scanned.
+	 * Adds the product to the shopping cart.
+	 * 
+	 * @param barcodeScanner The barcode scanner that cause the event
+	 * @param barcode The barcode of the scanned item
+	 */
+	@Override
+	public void aBarcodeHasBeenScanned(IBarcodeScanner barcodeScanner, Barcode barcode) {
+		if (!barcodeScanner.isDisabled() & inSession) {
+			station.baggingArea.addAnItem(null);
+		}
+		BarcodedProduct product = ProductDatabases.BARCODED_PRODUCT_DATABASE.get(barcode);
+		shoppingCart.add(product);
+	}
+	
+	private static void notifyAttendantStation() {}
+	@Override
+	public void aDeviceHasBeenEnabled(IDevice<? extends IDeviceListener> device) {}
+	@Override
+	public void aDeviceHasBeenDisabled(IDevice<? extends IDeviceListener> device) {}
+	@Override
+	public void aDeviceHasBeenTurnedOn(IDevice<? extends IDeviceListener> device) {}
+	@Override
+	public void aDeviceHasBeenTurnedOff(IDevice<? extends IDeviceListener> device) {}
+	@Override
+	public void theMassOnTheScaleHasExceededItsLimit(IElectronicScale scale) {}
+	@Override
+	public void theMassOnTheScaleNoLongerExceedsItsLimit(IElectronicScale scale) {}
 	
 	
 	/**
